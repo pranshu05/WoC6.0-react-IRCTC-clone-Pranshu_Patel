c4562dd7cd0334b1c5e5607250d2a5ca81b0bd2e
Pranshu Patel <pranshu05patel@gmail.com>
2024-01-17 16:31:50 +0530

    Add icons to sign-in and sign-up forms

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../firebase/auth';
 import { signInWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
+import { FaUserAlt, FaLock, FaSignInAlt } from 'react-icons/fa';
 
 const SignIn = () => {
     const navigate = useNavigate();
@@ -38,10 +39,12 @@ const SignIn = () => {
                 <div className='mx-auto w-11/12 sm:w-3/4 md:w-2/3 lg:w-2/5'>
                     <div className='backdrop-filter backdrop-blur-sm py-10 bg-black bg-opacity-5 border border-gray-500 rounded-lg flex flex-col'>
                         <h2 className="mb-4 text-2xl font-extrabold leading-none tracking-tight md:text-3xl lg:text-4xl text-white">Sign In</h2>
-                        <input className="bg-transparent border border-gray-500 my-2 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3" type="email" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} />
-                        <input className="bg-transparent border border-gray-500 my-2 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <label className="text-white flex justify-center mb-2 mt-10">Email address <FaUserAlt className='mt-1 mx-2' /></label>
+                        <input className="bg-transparent border border-gray-500 mb-2 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3" type="email" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <label className="text-white flex justify-center mb-2">Password <FaLock className='mt-1 mx-2' /></label>
+                        <input className="bg-transparent border border-gray-500 mb-10 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                         {error && <p className="text-red-500 my-4">{error}</p>}
-                        <button className="bg-black bg-opacity-5 hover:bg-opacity-10 border border-gray-500 my-2 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-1/3" onClick={handleSignIn}>Sign In</button>
+                        <button className="bg-black bg-opacity-5 hover:bg-opacity-10 border border-gray-500 mb-4 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-1/3 flex justify-center" onClick={handleSignIn}><FaSignInAlt className='mt-1 mx-2' /> Sign In</button>
                         <p className="text-gray-400">
                             Don't have an account? <a className='text-blue-500' href="/signup">Sign Up here</a>
                         </p>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../firebase/auth';
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
+import { FaUserAlt, FaLock, FaSignInAlt, } from 'react-icons/fa';
 
 const SignUp = () => {
     const navigate = useNavigate();
@@ -39,10 +40,12 @@ const SignUp = () => {
                 <div className='mx-auto w-11/12 sm:w-3/4 md:w-2/3 lg:w-2/5'>
                     <div className='backdrop-filter backdrop-blur-sm py-10 bg-black bg-opacity-5 border border-gray-500 rounded-lg flex flex-col'>
                         <h2 className="mb-4 text-2xl font-extrabold leading-none tracking-tight md:text-3xl lg:text-4xl text-white">Sign Up</h2>
-                        <input className="bg-transparent border border-gray-500 my-2 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3" type="email" label="Email address" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Email address" />
-                        <input className="bg-transparent border border-gray-500 my-2 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3" type="password" label="Create password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Password" />
+                        <label className="text-white flex justify-center mb-2 mt-10">Email address <FaUserAlt className='mt-1 mx-2' /></label>
+                        <input className="bg-transparent border border-gray-500 mb-2 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3" type="email" label="Email address" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Email address" />
+                        <label className="text-white flex justify-center mb-2">Password <FaLock className='mt-1 mx-2' /></label>
+                        <input className="bg-transparent border border-gray-500 mb-10 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3" type="password" label="Create password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Password" />
                         {error && <p className="text-red-500 my-4">{error}</p>}
-                        <button className="bg-black bg-opacity-5 hover:bg-opacity-10 border border-gray-500 my-2 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-1/3" onClick={handleSignUp}>Sign Up</button>
+                        <button className="bg-black bg-opacity-5 hover:bg-opacity-10 border border-gray-500 mb-4 rounded-lg p-2 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-1/3 flex justify-center" onClick={handleSignUp}><FaSignInAlt className='mt-1 mx-2'/> Sign Up</button>
                         <p className="text-gray-400">
                             Already have an account? <a className='text-blue-500' href="/signin">Sign In here</a>
                         </p>
